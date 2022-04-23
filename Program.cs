4863e85c6283a55817b4c3e535acf08bc57efb38
Bluscream <Bluscream@users.noreply.github.com>
2022-04-23 19:04:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ArgsToFile
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) return;
+            string argstr = string.Join(" ", args);
+            string path =Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "adb.log");
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(argstr);
+                }
+            } else {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(argstr); ;
+                }
+            }
+        }
+    }
+}
